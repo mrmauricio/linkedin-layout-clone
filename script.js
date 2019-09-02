7025f1dcbdc1fd1d13f3b410e9333e49c91d341f
mrmauricio <mendes-mauricio@outlook.com>
2019-09-02 20:52:12 +0100

    Added Page Loading

--- script.js
+++ script.js
@@ -56,3 +56,17 @@ function showRecommendationsInfo(element) {
         square.style.display = "block";
     }
 }
+
+function waitPageLoading() {
+    setTimeout(function() {
+        const loadingPage = document.getElementById("loading-page");
+        const [container] = document.getElementsByClassName("container");
+        const header = document.getElementById("main-header").firstElementChild;
+
+        loadingPage.style.display = "none";
+        header.style.display = "flex";
+        container.style.display = "grid";
+    }, 2000);
+}
+
+waitPageLoading();
